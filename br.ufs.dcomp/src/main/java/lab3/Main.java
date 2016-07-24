92cf0f83e17f1946fd645dabcd373d4f27c591bd
ericmguimaraes <ericmguimaraes@gmail.com>
2016-07-24 16:36:46 -0300

    comentarios

--- br.ufs.dcomp/src/main/java/lab3/Main.java
+++ br.ufs.dcomp/src/main/java/lab3/Main.java
@@ -19,7 +19,34 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException, PTStemmerException {
-       /* System.out.println("Google Search Agent: " + SearchAgent.getGoogleHits("vem monstro") + " results"
+        // Tarefa 01
+        FileManager fm = new FileManager();
+        List<Document> documents = new ArrayList<>();
+        System.out.println("******Lendo arquivos******");
+        documents.add(new Document("saude1",fm.readFileToString("corpus/corpus_saude1.txt"),"saude"));
+        documents.add(new Document("saude2",fm.readFileToString("corpus/corpus_saude2.txt"),"saude"));
+        documents.add(new Document("tecnologia1",fm.readFileToString("corpus/corpus_tecnologia1.txt"),"tecnologia"));
+        documents.add(new Document("tecnologia2",fm.readFileToString("corpus/corpus_tecnologia2.txt"),"tecnologia"));
+        documents.add(new Document("negocios1",fm.readFileToString("corpus/corpus_negocios1.txt"),"negocios"));
+        documents.add(new Document("negocios2",fm.readFileToString("corpus/corpus_negocios2.txt"),"negocios"));
+
+        // Tarefa 01
+        TermDocumentHelper termDocumentHelper = new TermDocumentHelper(documents);
+        System.out.println("******Preparando Matriz de Importancia TermoXDocumento******");
+        termDocumentHelper.init();
+        termDocumentHelper.printImportanceMatrix(false);
+        termDocumentHelper.printImportanceMatrix(true);
+        MachineLearningHandler machineLearningHandler = new MachineLearningHandler();
+        machineLearningHandler.knn("Importance_Matrix.csv",0,1,true);
+
+        //Tarefa 02
+        System.out.println("******Preparando Matriz de TF TermoXDocumento******");
+        termDocumentHelper.printTFMatrix(true);
+        termDocumentHelper.printTFMatrix(false);
+        machineLearningHandler.naiveBayes("TF_Matrix.csv",0,true);
+
+        //Tarefa 03
+        /* System.out.println("Google Search Agent: " + SearchAgent.getGoogleHits("vem monstro") + " results"
                 + "\nFilmow Search Agent: " + SearchAgent.getReview("filmow", "procurando-dory-t61563")
                 + "\nAdoro Cinema Search Agent: " + SearchAgent.getReview("adorocinema", "226078"));
 
@@ -33,28 +60,6 @@ public class Main {
         }
 
         */
-
-        // Tarefa 01
-        FileManager fm = new FileManager();
-        List<Document> documents = new ArrayList<>();
-        System.out.println("******Lendo arquivos******");
-        documents.add(new Document("saude1",fm.readFileToString("corpus/corpus_saude1.txt"),"saude"));
-        documents.add(new Document("saude2",fm.readFileToString("corpus/corpus_saude2.txt"),"saude"));
-        documents.add(new Document("tecnologia1",fm.readFileToString("corpus/corpus_tecnologia1.txt"),"tecnologia"));
-        documents.add(new Document("tecnologia2",fm.readFileToString("corpus/corpus_tecnologia2.txt"),"tecnologia"));
-        documents.add(new Document("negocios1",fm.readFileToString("corpus/corpus_negocios1.txt"),"negocios"));
-        documents.add(new Document("negocios2",fm.readFileToString("corpus/corpus_negocios2.txt"),"negocios"));
-        TermDocumentHelper termDocumentHelper = new TermDocumentHelper(documents);
-        System.out.println("******Preparando Matriz de Importancia TermoXDocumento******");
-        termDocumentHelper.init();
-        termDocumentHelper.printImportanceMatrix(false);
-        termDocumentHelper.printImportanceMatrix(true);
-        MachineLearningHandler machineLearningHandler = new MachineLearningHandler();
-        machineLearningHandler.knn("Importance_Matrix.csv",0,1,true);
-        System.out.println("******Preparando Matriz de TF TermoXDocumento******");
-        termDocumentHelper.printTFMatrix(true);
-        termDocumentHelper.printTFMatrix(false);
-        machineLearningHandler.naiveBayes("TF_Matrix.csv",0,true);
 
     }
 
